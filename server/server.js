830961453a5ea65b4f3171f7d7839bb888ce70a2
Ethan Lee <dalpong2014@gmail.com>
2022-12-23 19:25:09 -0500

    recent change

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
+
+//serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*,", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use(require("./routes/record.js"));
 // get driver connection
 const dbo = require("./db/conn");
